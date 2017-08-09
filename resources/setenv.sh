a0c0c256fc8d89faf60e34a00430765bdde9e642
Zopanix <zopanix@gmail.com>
2017-08-09 12:11:57 -0400

    Fixing variables

--- resources/setenv.sh
+++ resources/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-CATALINA_OPTS="$CATALINA_OPTS -javaagent:$PINPOINT_AGENT_PATH/pinpoint-agent/pinpoint-bootstrap-$PINPOINT_AGENT_VERSION.jar"
+CATALINA_OPTS="$CATALINA_OPTS -javaagent:$PINPOINTAGENT_PATH/pinpoint-agent/pinpoint-bootstrap-$PINPOINTAGENT_VERSION.jar"
 CATALINA_OPTS="$CATALINA_OPTS -Dpinpoint.agentId=$HOSTNAME"
 CATALINA_OPTS="$CATALINA_OPTS -Dpinpoint.applicationName=analyze-webapp_$ENV"
