415b5848809690114d75fc7d7eefda4dbbbc58dd
Zopanix <zopanix@gmail.com>
2017-08-10 08:49:56 -0400

    Making sure the id is max 24 char long

--- resources/setenv.sh
+++ resources/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 CATALINA_OPTS="$CATALINA_OPTS -javaagent:${PINPOINT_AGENT_PATH}/pinpoint-agent/pinpoint-bootstrap-${PINPOINTAGENT_VERSION}.jar"
-CATALINA_OPTS="$CATALINA_OPTS -Dpinpoint.agentId=${HOSTNAME}"
+CATALINA_OPTS="$CATALINA_OPTS -Dpinpoint.agentId=$(printf ${HOSTNAME} | tail -c 24)"
 CATALINA_OPTS="$CATALINA_OPTS -Dpinpoint.applicationName=${APPLICATION}-${ENV}"
